487e3d28e657f62a39e53227e70a8eb1e8f5462d
Gergely Brautigam <skarlso777@gmail.com>
2017-01-25 16:37:51 +0100

    Added facebook slim connector.

---
+++ facebook/index.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+require_once __DIR__ . '/vendor/autoload.php';
+$APP_ID = "<APP_ID>";
+$APP_SECRET = "<APP_SECRET>";
+
+$fb = new Facebook\Facebook([
+  'app_id' => $APP_ID,
+  'app_secret' => $APP_SECRET,
+  'default_graph_version' => 'v2.5',
+]);
+$helper = $fb->getRedirectLoginHelper();
+
+$app = new Slim\App();
+//$router = $app->router;
+
+$app->get('/login', function ($request, $response, $args) use ($helper) {
+    $permissions = ['user_posts']; // optional
+    $loginUrl = $helper->getLoginUrl('http://localhost:8000/callback', $permissions);
+    $response->write("<a href='". $loginUrl ."'>LoginWithFaceBook</a>");
+    return $response;
+});
+
+$app->get('/callback', function ($request, $response, $args) use ($app, $helper) {
+    try {
+      $accessToken = $helper->getAccessToken();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        $response->write("Exception occured:" . $e->getMessage());
+        return $response;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        $response->write("Exception occured:" . $e->getMessage());
+        return $response;
+    }
+
+    if (isset($accessToken)) {
+        $_SESSION['facebook_access_token'] = (string) $accessToken;
+    }
+    return $response->withRedirect('/list', 301);
+});
+
+$app->get('/list', function ($request, $response, $args) use ($fb, $helper) {
+    $token = $_SESSION['facebook_access_token'];
+
+    if (empty($token)) {
+        $response->write("Unauthorized");
+        return $response;
+    }
+
+    $fb->setDefaultAccessToken($token);
+    try
+    {
+        $resp = $fb->get('/me/posts');
+        $wholeResponse = json_encode((array)$resp);
+        $response->write($wholeResponse);
+        return $response;
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        $response->write($e->getMessage());
+        return $response;
+    }
+});
+
+$app->run();
